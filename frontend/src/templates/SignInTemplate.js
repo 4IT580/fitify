d47ca3cee34733dda13aafa904c32c487b8db783
Jana Jurčová <jj.jurcova@gmail.com>
2021-10-30 14:59:07 +0200

    Revert "back link color"
    This reverts commit 5eae6ffd4d44927161bbe79ca7248115565195e2.

--- frontend/src/templates/SignInTemplate.js
+++ frontend/src/templates/SignInTemplate.js
@@ -14,13 +14,13 @@ export function SignInTemplate({ isLoading, error, onSubmit }) {
           errorMessage={error && error.message}
           onSubmit={onSubmit}
           className="mt3"
-<<<<<<< HEAD
-        />
+        </>
       </RightBlockSection>
        <>
           <div className="lh-copy">
             or{' '}
             <Link className="f5 dark green" to={route.signUp()}>
+
               Sign Up
             </Link>
           </div>
