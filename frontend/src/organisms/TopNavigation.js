9fea979b0eb03a4c684ecadf6af6ffe44ad6767d
michondr <michondr@gmail.com>
2021-10-30 15:13:41 +0200

    fix text color

--- frontend/src/organisms/TopNavigation.js
+++ frontend/src/organisms/TopNavigation.js
@@ -15,27 +15,27 @@ export function TopNavigation() {
       <Link
         to={route.home()}
         noUnderline
-        className="b flex items-center pv2 ph3"
+        className="b flex items-center pv2 ph3 green"
       >
-        <Logo style={{ height:98, width: 110 }} className="mr1 f2"/>
+        <Logo style={{ height: 53, width: 36 }} className="mr2 f4" />
         Fitify
       </Link>
       <div className="flex-grow flex items-center">
-        <NavLink exact to={route.home()} className="pa3">
+        <NavLink exact to={route.home()} className="pa3 green">
           Home
         </NavLink>
-        <NavLink to={route.about()} className="pa3">
+        <NavLink to={route.about()} className="pa3 green">
           About
         </NavLink>
-        <NavLink to={route.foo()} className="pa3">
-          My foo page
+        <NavLink to={route.foo()} className="pa3 green">
+          my foo page
         </NavLink>
         {user ? (
           <>
             <NavLink
               to={route.userDetail(user.userName)}
               noUnderline
-              className="ph3 pv1 h-100 flex items-center"
+              className="ph3 pv1 h-100 flex items-center green"
             >
               <AvatarPhoto
                 className="v-mid dib mr2"
@@ -60,10 +60,10 @@ export function TopNavigation() {
           </>
         ) : (
           <>
-            <NavLink to={route.signIn()} className="pa3">
+            <NavLink to={route.signIn()} className="pa3 green">
               Sign In
             </NavLink>
-            <NavLink to={route.signUp()} className="pa3">
+            <NavLink to={route.signUp()} className="pa3 green">
               Sign up
             </NavLink>
           </>
