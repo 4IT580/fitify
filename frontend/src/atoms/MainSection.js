53b44b115d66a1f2c9ff6591cbefa1a7a22a58be
Jana Jurčová <jj.jurcova@gmail.com>
2021-10-31 21:03:45 +0100

    better version

--- frontend/src/atoms/MainSection.js
+++ frontend/src/atoms/MainSection.js
@@ -6,7 +6,7 @@ export function MainSection({ children }) {
     <>
       <main className="main-section ">
         <div className="cf right-offset">
-          <section className="fl w-100 bg-white o-60 pa5 mt7">
+          <section className="fl w-100 bg-white o-60 pa5 mt4">
             <div className="tl">{children}</div>
           </section>
         </div>
