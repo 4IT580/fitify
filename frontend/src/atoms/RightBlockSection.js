83b9a45eaf7a147e0d09f7b77b2cabc298bc1ed5
Jana Jurčová <jj.jurcova@gmail.com>
2021-11-11 15:50:43 +0100

    Changes for devices

--- frontend/src/atoms/RightBlockSection.js
+++ frontend/src/atoms/RightBlockSection.js
@@ -5,11 +5,11 @@ export function RightBlockSection({ children }) {
   return (
     <>
       <main className="home-page ">
-        <div className="cf right-offset">
+        <div className="cf right-offset vh-100 overflow-auto">
           <section className="fl w-60-l w-20-m w-100">
             <span>&nbsp;</span>
           </section>
-          <section className="fl w-30-l w-80-m w-100 bg-white br2 pa5">
+          <section className="fl w-30-l w-60-m w-100 bg-white br2 pa5 mb2">
             <div className="">{children}</div>
           </section>
         </div>
