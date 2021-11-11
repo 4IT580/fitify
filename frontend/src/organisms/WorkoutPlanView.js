a7b4dc18958bfc42e2665349c75ca004c6d831e6
Jana Jurčová <jj.jurcova@gmail.com>
2021-11-11 21:41:42 +0100

    # This is a combination of 3 commits. # This is the 1st commit message:
    workout history change
    
    # The commit message #2 will be skipped:
    
    # no message
    
    # The commit message #3 will be skipped:
    
    # Workout template

--- frontend/src/organisms/WorkoutPlanView.js
+++ frontend/src/organisms/WorkoutPlanView.js
@@ -12,7 +12,7 @@ import { route } from "../Routes";
 
 
 export function WorkoutPlanView ({planData, isLoading, error, refetch}) {
-  console.log(planData);
+
   let latestActiveWorkout = planData && planData.history
     .filter((historyItem) => (historyItem.status === 'active'))
     .sort(function (first, second) {
@@ -43,33 +43,33 @@ export function WorkoutPlanView ({planData, isLoading, error, refetch}) {
             <Heading size={'lg'} className={'green mt-2 nt4'}>
               {planData.name}
             </Heading>
-            <small className={'white'}>Created at: {fromUnixTimeStamp(planData.createdAt)}</small>
+            <small className={'green'}>Created at: {planData.createdAt}</small>
           </div>
 
 
           <div className={'cf'}>
             <div className={'w-100 w-50-l  fl'}>
-              <article className="hidden ba ma4">
-                <h1 className="f1 bg-dark white mv0 pv2 ph3">Train</h1>
-                <div className="pa3 bt bg-white tl">
-                  <p className={'f2 f5-ns dark'}>Interval length: {planData.intervalLength}</p>
-                  <p className={'f2 f5-ns dark'}>Interval pause length: {planData.intervalPauseLength}</p>
-                  <p className={'f2 f5-ns dark'}>rounds: {planData.rounds}</p>
-                  <p className={'f2 f5-ns dark'}>rounds pause length: {planData.roundsPauseLength}</p>
+              <article className="hidden ba ma4 br4">
+                <h1 className="f1 bg-dark green br4 mv0 pv2 ph3">Train</h1>
+                <div className="pa3 bt bg-dark o-70 tl">
+                  <p className={'f2 f5-ns green'}>Interval length: {planData.intervalLength}</p>
+                  <p className={'f2 f5-ns green'}>Interval pause length: {planData.intervalPauseLength}</p>
+                  <p className={'f2 f5-ns green'}>rounds: {planData.rounds}</p>
+                  <p className={'f2 f5-ns green'}>rounds pause length: {planData.roundsPauseLength}</p>
                   <Link className={'dib bg-animate pv2 br-pill bg-green dim ph5'} noUnderline={true} to={'/'}>Edit</Link>
                 </div>
               </article>
             </div>
 
             <div className={'w-100 w-50-l  fr'}>
-              <article className="hidden ba ma4">
-                <h1 className="f1 bg-dark white mv0 pv2 ph3">Exercises</h1>
+              <article className="hidden ba ma4 br4">
+                <h1 className="f1 bg-dark green br4 mv0 pv2 ph3">Exercises</h1>
                 {planData.exercises.map((exerciseItem) => (
-                  <div key={'exerciseItem' + exerciseItem.id} className="pa3 bt bg-white tl">
+                  <div key={'exerciseItem'+exerciseItem.id} className="pa3 bt bg-dark o-70 tl">
                     <Heading size={'md'} className={'green'}>
                       {exerciseItem.name}
                     </Heading>
-                    <p className="f2 f5-ns lh dark-copy measure mv0 pb3 dark">
+                    <p className="f2 f5-ns lh dark-copy measure mv0 pb3 green">
                       {exerciseItem.description}
                     </p>
                     <div className={'cf'}>
@@ -78,7 +78,7 @@ export function WorkoutPlanView ({planData, isLoading, error, refetch}) {
                           <>
                             <Heading size={'md'}>Body parts</Heading>
                             {exerciseItem.bodyParts.map((bodyPart) => (
-                              <p key={'bodyPart' + bodyPart.id} className="f2 f5-ns lh-copy measure mv0 dark">{bodyPart.name}</p>
+                              <p key={'bodyPart'+bodyPart.id} className="f2 f5-ns lh-copy measure mv0 green">{bodyPart.name}</p>
                             ))}
                           </>
                         )
@@ -91,7 +91,7 @@ export function WorkoutPlanView ({planData, isLoading, error, refetch}) {
                           <>
                             <Heading size={'md'}>Equipment you can use</Heading>
                             {exerciseItem.equipment.map((equipmentItem) => (
-                              <p key={'equipmentItem' + equipmentItem.id} className="f2 f5-ns lh-copy measure mv0 dark">{equipmentItem.name}</p>
+                              <p key={'equipmentItem'+equipmentItem.id} className="f2 f5-ns lh-copy measure mv0 green">{equipmentItem.name}</p>
                             ))}
                           </>
                         )
@@ -106,13 +106,14 @@ export function WorkoutPlanView ({planData, isLoading, error, refetch}) {
 
 
             <div className={'w-100 w-50-l  fl'}>
-              <article className="hidden ba ma4">
-                <h1 className="f1 bg-dark white mv0 pv2 ph3">Workout history</h1>
+              <article className="hidden ba ma4 br4">
+                <h1 className="f1 bg-dark green br4 mv0 pv2 ph3">Workout history</h1>
                 {planData.history.filter((historyItem) => (historyItem.status === 'finished')).map((historyItem) => (
-                  <div key={'historyItem' + historyItem.id} className="pa3 bt bg-white tl">
-                    <p className={'f2 f5-ns dark'}>From: {fromUnixTimeStamp(historyItem.startAt)}</p>
-                    <p className={'f2 f5-ns dark'}>{historyItem.endAt && ' Until: ' + fromUnixTimeStamp(historyItem.endAt)}</p>
-                    <p className={'f2 f5-ns dark'}>{historyItem.calories && ' Burnt calories: ' + historyItem.calories}</p>
+                  <div key={'historyItem'+historyItem.id} className="pa3 bt bg-dark o-70 tl">
+                    <p className={'f2 f5-ns green'}>Status: {historyItem.status}</p>
+                    <p className={'f2 f5-ns green'}>From: {historyItem.startAt}</p>
+                    <p className={'f2 f5-ns green'}>{historyItem.endAt && ' Until: ' + historyItem.endAt}</p>
+                    <p className={'f2 f5-ns green'}>{historyItem.calories && ' Burnt calories: ' + historyItem.calories}</p>
                     {historyItem.status === 'finished'
                     && (
                       <Link className={'dib bg-animate pv2 br-pill bg-green dim ph5'} noUnderline={true} to={'/'}>Repeat workout</Link>
