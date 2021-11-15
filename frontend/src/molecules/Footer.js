8ed3abe84d4a806f80d6cabe34c488ca58bccf06
michondr <michondr@gmail.com>
2021-11-15 05:53:33 +0100

    footer font fix

--- frontend/src/molecules/Footer.js
+++ frontend/src/molecules/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 export function Footer({}) {
   return (
     <>
-      <div className="footer bg-dark fixed bottom-0 w-100 green pa3 f2 f3-ns">
+      <div className="footer bg-dark fixed bottom-0 w-100 green pa3 f5-ns">
         <p>©2021 Fitify</p>
       </div>
     </>
