e0234549b6b760a8e4942cde78a39eb870db01c7
michondr <michondr@gmail.com>
2022-01-14 09:19:58 +0100

    run prettier

--- frontend/src/utils/types.js
+++ frontend/src/utils/types.js
@@ -1,17 +1,17 @@
 const types = {
-    ADD_EXERCISE: "add_exercise",
-    EDIT_EXERCISE: "edit_exercise",
-    UPDATE_EXERCISES: "update_exercises",
-    SET_START_TIME: "set_start_time",
-    UPDATE_WORK_TIME: "update_work_time",
-    UPDATE_REST_TIME: "update_rest_time",
-    UPDATE_SETS: "update_sets",
-    UPDATE_THEME: "update_theme",
-    UPDATE_SOUND_MODE: "update_sound_mode",
-    IS_CONFIG_VALID: "is_config_valid",
-    IS_EXERCISE_INPUT_VALID: "is_exercise_input_valid",
-    IS_RADIAL_COUNTER_ON: "is_radial_counter_on",
-    INITIAL_STATE: "initial"
+  ADD_EXERCISE: 'add_exercise',
+  EDIT_EXERCISE: 'edit_exercise',
+  UPDATE_EXERCISES: 'update_exercises',
+  SET_START_TIME: 'set_start_time',
+  UPDATE_WORK_TIME: 'update_work_time',
+  UPDATE_REST_TIME: 'update_rest_time',
+  UPDATE_SETS: 'update_sets',
+  UPDATE_THEME: 'update_theme',
+  UPDATE_SOUND_MODE: 'update_sound_mode',
+  IS_CONFIG_VALID: 'is_config_valid',
+  IS_EXERCISE_INPUT_VALID: 'is_exercise_input_valid',
+  IS_RADIAL_COUNTER_ON: 'is_radial_counter_on',
+  INITIAL_STATE: 'initial',
 };
 
 export default types;
