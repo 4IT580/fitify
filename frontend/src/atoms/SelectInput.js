c54dd9c4e3523e7b3ad45c510501ac60a4197d33
michondr <michondr@gmail.com>
2022-01-16 12:16:22 +0100

    dashboard filters

---
+++ frontend/src/atoms/SelectInput.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import classNames from 'classnames';
+
+export function SelectInput ({className, error, selectOptions, ...props}) {
+  return (
+    <select
+      className={classNames(
+        'border-box input-reset ba pa2 db w-100 mt2 mb2',
+        error ? 'border-red' : 'border-green',
+        className,
+      )}
+      {...props}
+    >
+      {selectOptions.map((option) => {
+        return (<option value={option.value}>
+          {option.name}
+        </option>)
+      })
+      }
+    </select>
+  );
+}
--- frontend/src/templates/DashboardTemplate.js
+++ frontend/src/templates/DashboardTemplate.js
@@ -13,39 +13,47 @@ import {
 
 import { PageLayout, DashboardTrainingCard } from 'src/organisms/';
 import { route } from 'src/Routes';
+import { SelectInput } from "../atoms/SelectInput";
+import { intervalMatches, nameMatches, pauseMatches } from "../utils/workoutFilterMatch";
 
-export function DashboardTemplate({ data, isLoading, error, refetch }) {
+export function DashboardTemplate ({data, isLoading, error, refetch}) {
   const [workoutData, setWorkoutData] = useState([]);
+  const [workoutFilter, setWorkoutFilter] = useState({
+    name: '',
+    interval: '',
+    pause: '',
+  });
 
-  function normalizeString(string) {
-    return string
-      .normalize('NFD')
-      .replace(/[\u0300-\u036f]/g, '')
-      .toLowerCase();
-  }
-
-  function filterWorkoutDataBasedOnValue(searchedValue) {
-    if (isLoading === true) {
-      setWorkoutData([]);
-    } else {
-      setWorkoutData(
-        data.user.workouts.filter((item) => {
-          if (searchedValue === undefined) {
-            return true;
-          }
-
-          return normalizeString(item.name).includes(
-            normalizeString(searchedValue),
-          );
-        }),
-      );
-    }
+  function handleFilter (evt) {
+    setWorkoutFilter({
+      ...workoutFilter,
+      [evt.target.name]: evt.target.value
+    });
   }
 
   useEffect(() => {
-    filterWorkoutDataBasedOnValue();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [isLoading, data]);
+    if (isLoading === true) {
+      return
+    }
+
+    setWorkoutData(
+      data.user.workouts.filter((item) => {
+        if (!nameMatches(workoutFilter, item.name)) {
+          return false
+        }
+
+        if (!intervalMatches(workoutFilter, item.intervalLength)) {
+          return false;
+        }
+
+        if (!pauseMatches(workoutFilter, item.intervalPauseLength)) {
+          return false;
+        }
+
+        return true;
+      }),
+    );
+  }, [isLoading, data, workoutFilter])
 
   return (
     <>
@@ -66,19 +74,57 @@ export function DashboardTemplate({ data, isLoading, error, refetch }) {
                   New Training
                 </Link>
               </Button>
-              <Label className={'green mt4'}>Filter trainings by name</Label>
-              <TextInput
-                className={'mw5-l ml-auto-l tr-l'}
-                placeholder={'name...'}
-                id={'filterInput'}
-                onChange={(e) => {
-                  filterWorkoutDataBasedOnValue(e.target.value);
-                }}
-              />
+              <div className={'dt-l dt--fixed'}>
+                <div className={'dtc-l ph3'}>
+                  <Label className={'green mt4'} htmlFor={'filterName'}>Filter trainings by name</Label>
+                  <TextInput
+                    className={'mw5-l ml-auto-l '}
+                    placeholder={'name...'}
+                    id={'filterName'}
+                    name={'name'}
+                    value={workoutFilter.name}
+                    onChange={handleFilter}
+                  />
+                </div>
+                <div className={'dtc-l ph3'}>
+                  <Label className={'green mt4'} htmlFor={'filterInterval'}>Filter trainings by interval</Label>
+                  <SelectInput
+                    className={'mw5-l ml-auto-l tr-l'}
+                    id={'filterInterval'}
+                    name={'interval'}
+                    value={workoutFilter.interval}
+                    selectOptions={[
+                      {name: '', value: ''},
+                      {name: 'Less than 20s', value: 'lessThan20'},
+                      {name: '20-30s', value: '20to30'},
+                      {name: '30-60s', value: '30to60'},
+                      {name: 'More than 60s', value: 'moreThan60'},
+                    ]}
+                    onChange={handleFilter}
+                  />
+                </div>
+                <div className={'dtc-l ph3'}>
+                  <Label className={'green mt4'} htmlFor={'filterPause'}>Filter trainings by pause</Label>
+                  <SelectInput
+                    className={'mw5-l ml-auto-l tr-l'}
+                    id={'filterPause'}
+                    name={'pause'}
+                    value={workoutFilter.pause}
+                    selectOptions={[
+                      {name: '', value: ''},
+                      {name: 'Less than 10s', value: 'lessThan10'},
+                      {name: '10-20s', value: '10to20'},
+                      {name: 'More than 20s', value: 'moreThan20'},
+                    ]}
+                    onChange={handleFilter}
+                  />
+                </div>
+              </div>
+
             </div>
           </div>
 
-          {(isLoading || !workoutData) && <Loading />}
+          {(isLoading || !workoutData) && <Loading/>}
 
           {error && (
             <ErrorBanner title={error.message}>
@@ -97,10 +143,7 @@ export function DashboardTemplate({ data, isLoading, error, refetch }) {
                 {workoutData
                   .filter((item) => item.isArchived === false)
                   .map((item, index) => (
-                    <DashboardTrainingCard
-                      item={item}
-                      key={'trainings' + index}
-                    />
+                    <DashboardTrainingCard item={item} key={'trainings' + index}/>
                   ))}
               </div>
               <Heading size={'xl'} className={'green pb4 mt4'}>
@@ -110,10 +153,7 @@ export function DashboardTemplate({ data, isLoading, error, refetch }) {
                 {workoutData
                   .filter((item) => item.isArchived === true)
                   .map((item, index) => (
-                    <DashboardTrainingCard
-                      item={item}
-                      key={'archived-trainings' + index}
-                    />
+                    <DashboardTrainingCard item={item} key={'archived-trainings' + index}/>
                   ))}
               </div>
             </div>
